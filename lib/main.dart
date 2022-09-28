a32c23d0c8b090f740ac93fd570b5f898a4c4e23
JennyChang1124 <athome889@gmail.com>
2022-09-28 18:19:34 +0800

    myfirstapp_0921

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'package:flutter/material.dart';
 void main() {
   var appTitle = Text('我的第一個Flutter App'),
       hiFlutter = Text('Hi,Flutter.\n今天是20220926',
-        style: TextStyle(fontSize: 30),
+        style: TextStyle(fontSize: 30)
        );
   var img=Image.network("https://tyenews.com/wp-content/uploads/2021/05/%E7%8B%97%E7%8B%975411953.jpg");
-  var appBody = Center(
-    // child: img,
+  var appBody = Container(
     child:hiFlutter,
+    alignment: Alignment.topRight,
+    margin:EdgeInsets.all(50),
+    color: Colors.white,
+    padding: EdgeInsets.fromLTRB(30, 30, 30, 30),
   );
   var appBar = AppBar(
     title: appTitle,
